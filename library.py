f09c1cac9e9f781051f14b3447f8d1175b4ec17e
Mateusz <szczepan.mateusz@gmail.com>
2022-05-27 21:29:14 +0200

    Library for excell service

--- library.py
+++ library.py
@@ -6,8 +6,22 @@ def open_file(path):
     return wb
 
 
-def read_file(wb, sheet, column, start_row, end_row):
+def open_sheet(wb, sheet):
     ws = wb[sheet]
+    return ws
+
+
+def read_file(ws, column, start_row, end_row):
     read_list = [ws[column + str(row)].value for row in range(start_row, end_row)]
     return read_list
+
+
+def write_file(ws, column_nr, column_sym, start_row, read_list):
+    ws.insert_cols(column_nr)
+    row_nr = start_row
+    for row_value in read_list:
+        ws[column_sym + row_nr] = row_value
+        row_nr = row_nr + 1
+
+
 
